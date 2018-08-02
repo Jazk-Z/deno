ec3634361fbdeb1d2b2f7975db4d4aa6f440b1da
Ryan Dahl <ry@tinyclouds.org>
2018-08-02 11:42:17 -0400

    Fix setMainSourceMap signature

--- js/lib.deno.d.ts
+++ js/lib.deno.d.ts
@@ -5,6 +5,7 @@
 /// <reference no-default-lib="true"/>
 
 /// <reference lib="esnext" />
+import { RawSourceMap } from "source-map";
 
 // TODO generate `console.d.ts` and inline it in `assets.ts` and remove
 // declaration of `Console`
@@ -29,7 +30,7 @@ interface Window {
   console: Console;
   // TODO(ry) These shouldn't be global.
   mainSource: string;
-  setMainSourceMap(sm: string): void;
+  setMainSourceMap(sm: RawSourcemap): void;
 }
 
 // Globals in the runtime environment
@@ -38,4 +39,4 @@ declare const window: Window;
 
 // TODO(ry) These shouldn't be global.
 declare let mainSource: string;
-declare function setMainSourceMap(sm: string): void;
+declare function setMainSourceMap(sm: RawSourcemap): void;
